b424267f69f41efaf4b67018be374807ebf20f5c
shangwen <shangwen>
2023-11-09 06:34:36 +0000

    init version

---
+++ databricks/notebook_01.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+# Importing necessary libraries
+from pyspark.sql.functions import countDistinct
+
+# Reading the cars table
+cars = spark.table("cars")
+
+# Getting the number of unique car types
+unique_types_count = cars.select(countDistinct("Type")).collect()[0][0]
+
+# Printing the result
+print("汽车类型数为：", unique_types_count)
+
+# COMMAND ----------
+
+# Importing necessary libraries
+from pyspark.sql.functions import countDistinct
+
+# Reading the cars table
+cars = spark.table("cars")
+
+# Getting the number of unique car types
+unique_types_count = cars.select(countDistinct("Type")).collect()[0][0]
+
+# Printing the result
+print("汽车类型数为：", unique_types_count)
